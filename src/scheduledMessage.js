14e8391370b744c94c6308cc31742577991b002f
Jakub Żmuda <goodguykuba@gmail.com>
2018-11-05 11:01:05 +0100

    fixed scheduler

--- src/scheduledMessage.js
+++ src/scheduledMessage.js
@@ -4,17 +4,22 @@ const absenceResponse = require('./absenceResponse');
 
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [1, 2, 3, 4, 5];
-rule.hour = 9;
+rule.hour = 8;
 rule.minute = 0;
 
 postAbsencesMessage = () => {
+  console.log(new Date(), 'posting daily message');
   absenceResponse((text) => {
     axios.post(process.env.SLACK_HOOK, {
       text: text,
       mrkdwn: true
     })
+        .then((res) => console.log(new Date(), 'posted daily message, res: ', res.data))
+        .catch((res) => console.log(new Date(), 'failed to post daily message, res: ', res.data))
   });
 };
+
+console.log(new Date(), 'scheduled post daily message');
 
 schedule.scheduleJob(rule, () => {
   postAbsencesMessage();
