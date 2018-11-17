21e2bde9c647b6ba9c07c36c7dc0139dcca19aa2
Jakub Żmuda <goodguykuba@gmail.com>
2018-11-17 11:40:58 +0100

    extracted db connectivity logic

---
+++ src/absenceRepository.js
@@ -0,0 +1,29 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
+const save = (absence, callback) => {
+  MongoClient.connect(process.env.DB_URI + process.env.DB_NAME, async (err, client) => {
+    const db = client.db(process.env.DB_NAME);
+    db.collection('absences').save(absence, callback);
+  });
+};
+
+const loadByUser = (user, callback) => {
+  MongoClient.connect(process.env.DB_URI, (err, client) => {
+    const db = client.db('absencebot');
+    db.collection('absences').find({ user: user }).toArray(callback);
+  });
+};
+
+const loadByDate = (date, callback) => {
+  MongoClient.connect(process.env.DB_URI, (err, client) => {
+    const db = client.db('absencebot');
+    db.collection('absences').find({ date: date}).toArray(callback);
+  });
+};
+
+module.export = {
+  save,
+  loadByUser,
+  loadByDate
+};
--- src/commands/myAbsences.js
+++ src/commands/myAbsences.js
@@ -1,22 +1,17 @@
-const MongoClient = require('mongodb').MongoClient;
 const moment = require('moment');
 const time = require('../time');
-const _ = require('lodash');
+const AbsenceRepository = require('../absenceRepository');
 
 const statusCommand = (req, res, intent) => {
-  MongoClient.connect(process.env.DB_URI, (err, client) => {
-    const db = client.db('absencebot');
-    db.collection('absences').find({ user: req.body.user_name }).toArray((err, result) => {
 
-      const entriesText = result
-        .filter(entry => moment(entry.date, time.dateFormat).diff(moment().format(time.dateFormat), 'days') >= 0)
-        .reduce((acc, entry) => {
-          return acc + `- *${entry.date}* #${entry.tag} ${note(entry.note)}\n`
-        }, '');
-
-      respondWithText(res, `*Your absences:*\n${entriesText}`);
-    });
-  })
+  AbsenceRepository.loadByUser(req.body.user_name, (err, result) => {
+    const entriesText = result
+      .filter(entry => moment(entry.date, time.dateFormat).diff(moment().format(time.dateFormat), 'days') >= 0)
+      .reduce((acc, entry) => {
+        return acc + `- *${entry.date}* #${entry.tag} ${note(entry.note)}\n`
+      }, '');
+    respondWithText(res, `*Your absences:*\n${entriesText}`);
+  });
 };
 
 const note = (noteText) => {
