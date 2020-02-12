411b511c1487b1e86627e3aa0c4a480446153519
Jakub Żmuda <goodguykuba@gmail.com>
2020-02-12 11:57:18 +0100

    warsaw timezone

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment';
+import * as moment from 'moment-timezone';
 import { dateFormat } from './time';
 
 export function parseIntentForSign(sign: string, intent: string, breaker = ' ') {
@@ -13,11 +13,11 @@ export function parseIntentForSign(sign: string, intent: string, breaker = ' ') 
 
 export function parseDateIntent(dateIntent: string): moment.Moment | undefined {
   if (dateIntent === 'today') {
-    return moment()
+    return moment().tz('Europe/Warsaw')
   } else if (dateIntent === 'tomorrow') {
-    return moment().add(1, 'days');
+    return moment().tz('Europe/Warsaw').add(1, 'days');
   } else if (dateIntent.match(/\d{4}\/\d{1,2}\/\d{1,2}/)) {
-    return moment(dateIntent, dateFormat);
+    return moment(dateIntent, dateFormat).tz('Europe/Warsaw');
   }
   return undefined;
 }
